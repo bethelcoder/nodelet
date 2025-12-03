6422053122c042d1ba89314b17bc0be6d29efe6a
bethelcoder <nduvheni066@gmail.com>
2025-12-03 15:14:50 +0200

    Initial project setup(NextJS, Prisma, Neon, tRPC)

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+
+import { getQueryClient, trpc } from "@/trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+  
+  return (
+    <main className="min-h-screen min-w-screen flex items-center justify-center">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>loading....</p>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
+    </main>
+  )
+}
+export default Page
